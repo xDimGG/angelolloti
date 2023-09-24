7eb0b3216089a3593b0ade6b7e44a8b1bc941096
Angelo Lloti <angelolloti@Angelos-MacBook-Air.local>
2023-09-23 22:00:40 -0400

    with fullpage

---
+++ src/lib/works.js
@@ -0,0 +1,14 @@
+export default [
+	{
+		name: 'Card Game App',
+		href: 'https://cg.dim.codes/',
+		description: 'A multiplayer online tabletop game simulator created using Vue and Go.',
+		image: '/cg.png',
+	},
+	{
+		name: 'Happy Island Website',
+		href: 'https://hisc.dim.codes/',
+		description: 'A commercial website for a senior care center built using Svelte, Node.js, and GitHub Pages.',
+		image: '/hisc.png',
+	},
+];
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+import Sections from './Sections.svelte';
+
+let scroll, hoverScroll, control;
+// $: console.log(control);
+
+const moveTo = ({ offsetY }) => window.scrollTo({ top: offsetY - window.innerHeight / 2 });
+const updateHover = ({ offsetY }) =>
+  hoverScroll = Math.min(document.body.scrollHeight - window.innerHeight / 2, Math.max(window.innerHeight / 2, offsetY)) - window.innerHeight / 2;
+</script>
+
+<svelte:window bind:scrollY={scroll} />
+
+<Sections control={control} />
+
+<!-- don't render mini view until first swiper has initialized -->
+<!-- <div
+  class="hidden md:block sections mini-view"
+  style={`--scrollbar-offset: ${scroll}px; --hover-scrollbar-offset: ${hoverScroll}px`}
+  on:click|preventDefault={moveTo}
+  on:mousemove={updateHover}
+  role="none"
+>
+  <Sections bind:swiper={control} control={null} />
+</div> -->
