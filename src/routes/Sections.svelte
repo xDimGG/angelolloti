40aaccad876241dd8e811921e3cddc6f2884ad12
Angelo Lloti <skyuphigh01@gmail.com>
2026-01-15 22:19:58 -0500

    fix

--- src/routes/Sections.svelte
+++ src/routes/Sections.svelte
@@ -52,7 +52,7 @@ register();
 				<p class="text-slate-400 text-opacity-70 text-2xl animation-fadein animation-3 indent-2">Hardware Design</p>
 				<p class="text-slate-400 text-opacity-50 text-2xl animation-fadein animation-4 indent-2">Engineering Consulting</p>
 			</div>
-			<div class="max-w-[80%] md:max-h-[60vh] animation-fadein animation-1 md:animation-2 links">
+			<!-- <div class="max-w-[80%] md:max-h-[60vh] animation-fadein animation-1 md:animation-2 links">
 				<img src="/me.jpeg" alt="Angelo Lloti" class="shadow-[0_0_40px_-10px_#e0e2e6] w-100% max-h-[50vh] md:max-h-[60vh] rounded-full relative z-10">
 				<div class="w-100 flex items-center justify-around pt-3">
 					<a href="/Angelo_Lloti_Resume.pdf" class="fill-black bg-white p-4 rounded-full -mt-14 sm:-mt-20">
@@ -65,7 +65,7 @@ register();
 						<svg class="w-12 sm:w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
 					</a>
 				</div>
-			</div>
+			</div> -->
 		</div>
 		<div class="fill-gray-400 hover:fill-white transition-colors animate__animated animate__bounce animate__delay-5s py-5 cursor-pointer" on:keypress={goDown} on:click={goDown} role="button" tabindex="0">
 			<svg width="40" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 12c0-6.623-5.377-12-12-12s-12 5.377-12 12 5.377 12 12 12 12-5.377 12-12zm-1 0c0-6.071-4.929-11-11-11s-11 4.929-11 11 4.929 11 11 11 11-4.929 11-11zm-11.5 4.828l-3.763-4.608-.737.679 5 6.101 5-6.112-.753-.666-3.747 4.604v-11.826h-1v11.828z"/></svg>
